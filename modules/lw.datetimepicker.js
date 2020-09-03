f6d44942928bfafcc9c89a5a0096b33b8cc522dc
Andy Keren <andrey@luwfy.com>
2020-09-03 15:21:46 +0300

    module tesdt

---
+++ modules/lw.datetimepicker.js
@@ -0,0 +1,24 @@
+
+/* Smart UI v7.7.1 (2020-July) 
+Copyright (c) 2011-2020 jQWidgets. 
+License: https://htmlelements.com/license/ */ //
+
+import "../lw.element.js";
+import "../lw.button.js";
+import "../lw.date.js";
+import "../lw.scrollbar.js";
+import "../lw.listbox.js";
+import "../lw.dropdownlist.js";
+import "../lw.draw.js";
+import "../lw.tooltip.js";
+import "../lw.timepicker.js";
+import "../lw.calendar.js";
+import "../lw.numeric.js";
+import "../lw.math.js";
+import "../lw.datetimepicker.js";
+
+export class lwDateTimePicker extends LW.Component {
+	get name() {
+		return 'lwDateTimePicker';
+	}		
+}
